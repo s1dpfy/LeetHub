2e76bf9183ed83a389f30d6ceff24eb3ddb4efb6
양다니엘 <eldotemp01@gmail.com>
2026-07-14 21:02:48 +0900

    Time: 79 ms (84.42%), Space: 16.6 MB (8.58%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -6,4 +6,29 @@ class Solution(object):
         :type wordList: List[str]
         :rtype: int
         """
-        
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+
+        pattern_dict = defaultdict(list)
+        for word in wordSet:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                pattern_dict[pattern].append(word)
+
+        queue = deque([(beginWord, 1)])  
+        visited = set([beginWord])
+
+        while queue:
+            word, steps = queue.popleft()
+            if word == endWord:
+                return steps
+
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                for neighbor in pattern_dict.get(pattern, []):
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append((neighbor, steps + 1))
+
+        return 0
